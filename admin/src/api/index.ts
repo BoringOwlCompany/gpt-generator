152b6ea37dd03bcb793194a1294e4244e80623fc
dawid-jazdzewski <dawid.jazdzewski@boringowl.io>
2023-03-23 13:38:52 +0100

    feat

--- admin/src/api/index.ts
+++ admin/src/api/index.ts
@@ -1,21 +1,21 @@
 import { request } from "@strapi/helper-plugin";
 
 import type {
-  IGenerateArticleRequest,
-  IGeneratedArticleResponse,
+    IGenerateArticleRequest,
+    IGeneratedArticleResponse,
 } from "../types";
 
 export const api = {
-  generateArticle: async (data: IGenerateArticleRequest) => {
-    return await request(`/generator`, {
-      method: "POST",
-      body: data,
-    });
-  },
-  save: async (data: IGeneratedArticleResponse) => {
-    return await request(`/generator/save`, {
-      method: "POST",
-      body: data,
-    });
-  },
+    generateArticle: async (data: IGenerateArticleRequest) => {
+        return await request(`/gpt-generator`, {
+            method: "POST",
+            body: data,
+        });
+    },
+    save: async (data: IGeneratedArticleResponse) => {
+        return await request(`/gpt-generator/save`, {
+            method: "POST",
+            body: data,
+        });
+    },
 };
--- server/openai/openai.ts
+++ server/openai/openai.ts
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const config = new Configuration({
-  apiKey: process.env.GPT_API_KEY,
+    apiKey: process.env.GPT_API_KEY,
 });
 const openai = new OpenAIApi(config);
 
