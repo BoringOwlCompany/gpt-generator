5e185b0f2b71f56ac1c9b48528282066d1e9f668
dawid-jazdzewski <dawid.jazdzewski@boringowl.io>
2023-03-23 12:26:55 +0100

    feat

--- config/plugins.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  // ...
-  generator: {
-    enabled: true,
-    resolve: "./src/plugins/generator",
-  },
-  // ...
-};
--- src/plugins/generator/README.md
+++
@@ -1,3 +0,0 @@
-# Strapi plugin generator
-
-A quick description of generator.
